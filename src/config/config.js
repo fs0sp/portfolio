266908b317c31f7c87c42155196a97a7961a38e8
Shivam <shivampokhriyal@rocketmail.com>
2023-02-08 12:12:15 +0530

    added attire project

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,14 @@ export const projectDetails = [
     "desc": "Our website allows you to easily input and track your 1 rep max weights for a variety of exercises, including bench press, squat, and deadlift. With our user-friendly interface, you can see your progress over time and adjust your training accordingly.",
   },
   {
+    "title": "QKart",
+    "image": "https://i.imgur.com/5Ia6Yx0.png",
+    "github": "https://github.com/CrioDo/PROGRAM_DEVSPRINT_1611299302892-itechshivam2-ME_QKART_FRONTEND",
+    "link": "https://q-kart-frontend-app.netlify.app/",
+    "tech": ["ReactJs"],
+    "desc": "QKart is an E-commerce application offering a variety of products for customers to choose from. Implemented the core logic for authentication, shopping cart and checkout. Improved UI by adding responsive design elements for uniform experience across different devices. Utilized REST APIs to dynamically load and render data served by the backend server",
+  },
+  {
     "title": "Codefest.Sh",
     "image": "https://i.imgur.com/hyikqdz.png",
     "github": "https://github.com/fs0sp/codefest",
@@ -16,13 +24,14 @@ export const projectDetails = [
     "desc": "Codefest is a premier destination for competitive programmers and coding enthusiasts, providing a comprehensive platform to discover and participate in coding contests from top websites like Codeforces, Leetcode and more.",
   },
   {
-    "title": "QKart",
-    "image": "https://i.imgur.com/5Ia6Yx0.png",
-    "github": "https://github.com/CrioDo/PROGRAM_DEVSPRINT_1611299302892-itechshivam2-ME_QKART_FRONTEND",
-    "link": "https://q-kart-frontend-app.netlify.app/",
-    "tech": ["ReactJs"],
-    "desc": "QKart is an E-commerce application offering a variety of products for customers to choose from. Implemented the core logic for authentication, shopping cart and checkout. Improved UI by adding responsive design elements for uniform experience across different devices. Utilized REST APIs to dynamically load and render data served by the backend server",
-  }
+    "title": "Attire",
+    "image": "https://i.imgur.com/s71aJMF.png",
+    "github": "https://github.com/fs0sp/attire",
+    "link": "https://attireshop.netlify.app/",
+    "tech": ["ReactJS", "TailwindCSS", "Redux-Toolkit"],
+    "desc": "A shopping cart website made using ReactJS, TailwindCSS, and Redux-Toolkit. Implemented state persistence using local storage. ",
+  },
+  
 ];
 
 export const navBarLinks = [
